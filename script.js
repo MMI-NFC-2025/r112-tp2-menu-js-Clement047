fa8d73a2dac9b95a9fd2066977577289d43515e0
Clement047 <clement.bertrand02@edu.univ-fcomte.fr>
2025-11-12 18:22:44 +0100

    ouverture et fermeture du menu

---
+++ script.js
@@ -0,0 +1,10 @@
+// on cible les éléments à modifier
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+
+//  Ajoute un écouteur d'évènements à l'élément 'toggle' qui écoute les évènements 'click'
+toggle.addEventListener( "click", () => {
+    // Mise à jour des attributs ARIA pour acessibilité
+    nav.ariaHidden = nav.ariaHidden === "false";
+    toggle.ariaExpanded = toggle.ariaExpanded === "true";
+})
