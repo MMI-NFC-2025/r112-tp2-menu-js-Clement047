681698e57cdf172abf08c35d00e27f5815399462
Clement047 <clement.bertrand02@edu.univ-fcomte.fr>
2025-11-14 09:05:10 +0100

    noscroll

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// on cible les éléments à modifier
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
-
-//  Ajoute un écouteur d'évènements à l'élément 'toggle' qui écoute les évènements 'click'
-toggle.addEventListener( "click", () => {
-    // Mise à jour des attributs ARIA pour acessibilité
+toggle.addEventListener("click", () => {
     nav.ariaHidden = nav.ariaHidden === "false";
-    toggle.ariaExpanded = toggle.ariaExpanded === "true";
+    toggle.ariaExpended = toggle.ariaExpended === "true";
     document.body.classList.toggle("noscroll");
 });
